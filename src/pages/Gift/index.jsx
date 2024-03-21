b24c25ca2282b4810a87fd6dfa4a1766a9279bb5
r.boikov <r.boikov@wsc-sports.com>
2024-03-21 22:08:27 +0200

    Fixed gifts page

--- src/pages/Gift/index.jsx
+++ src/pages/Gift/index.jsx
@@ -1,32 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { Louder } from '../../components/Louder';
+import { Louder } from "../../components/Louder";
 
-import { getCareers, deleteCareers } from './actions';
+import { getGifts, deleteGift } from "./actions";
 
 export const GiftPage = () => {
-  const [career, setCareers] = useState([]);
+  const [gifts, setGifts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [forceUpdate, setForceUpdate] = useState(false);
 
   const toggleForceUpdate = () => setForceUpdate((fU) => !fU);
 
-  const onDeleteCareer = (id) => {
-    const confirmed = window.confirm('Are you sure you want to delete the contact permanently?');
+  const onDeleteGift = (id) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete the gift permanently?"
+    );
 
     if (confirmed) {
       setLoading(true);
-      deleteCareers({ id }).then(() => {
+      deleteGift({ id }).then(() => {
         setLoading(false);
         toggleForceUpdate();
       });
     }
-  }
+  };
 
   useEffect(() => {
     setLoading(true);
-    getCareers().then((data) => {
-      setCareers(data.gift);
+    getGifts().then((data) => {
+      setGifts(data.gift);
       setLoading(false);
     });
   }, [forceUpdate]);
@@ -35,15 +37,21 @@ export const GiftPage = () => {
     <div className="career-page">
       <Louder visible={loading} />
       <div className="_mt-8">
-        {career.map((el, i) => (
+        {gifts.map((el, i) => (
           <div className="card _mb-3" key={el.id}>
             <div className="card-header d-flex justify-content-between align-items-center">
-              <h5 className="card-title mb-0">{'Gift ' + (i + 1)}</h5>
-              <button type="button" className="btn btn-danger" onClick={() => onDeleteCareer(el.id)}>x</button>
+              <h5 className="card-title mb-0">{"Gift " + (i + 1)}</h5>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={() => onDeleteGift(el.id)}
+              >
+                x
+              </button>
             </div>
             <div className="card-body">
-              <p className="card-text">Phone: {el.fEmail}</p>
-              <p className="card-text">Email: {el.fPhone}</p>
+              <p className="card-text">Phone: {el.email}</p>
+              <p className="card-text">Email: {el.phone}</p>
               <p className="card-text">About: {el.comment}</p>
             </div>
           </div>
