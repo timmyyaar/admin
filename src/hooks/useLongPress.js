ff240f5f642a1a5e846bf93ede036de3ac40db0f
r.boikov <r.boikov@wsc-sports.com>
2024-04-06 10:43:01 +0300

    Mobile tests

--- src/hooks/useLongPress.js
+++ src/hooks/useLongPress.js
@@ -15,38 +15,33 @@ const preventDefault = (event) => {
 const useLongPress = (
   onLongPress,
   onClick,
-  { shouldPreventDefault = true, delay = 300 } = {},
-  target
+  { shouldPreventDefault = true, delay = 300 } = {}
 ) => {
   const [longPressTriggered, setLongPressTriggered] = useState(false);
   const timeout = useRef();
+  const target = useRef();
 
-  const start = useCallback(
-    (event) => {
-      if (shouldPreventDefault) {
-        target.current.addEventListener("touchend", preventDefault, {
-          passive: false,
-        });
-      }
-      timeout.current = setTimeout(() => {
-        onLongPress(event);
-        setLongPressTriggered(true);
-      }, delay);
-    },
-    [onLongPress, delay, shouldPreventDefault]
-  );
+  const start = (event) => {
+    if (shouldPreventDefault && event.target) {
+      event.target.addEventListener("touchend", preventDefault, {
+        passive: false,
+      });
+      target.current = event.target;
+    }
+    timeout.current = setTimeout(() => {
+      onLongPress(event);
+      setLongPressTriggered(true);
+    }, delay);
+  };
 
-  const clear = useCallback(
-    (event, shouldTriggerClick = true) => {
-      timeout.current && clearTimeout(timeout.current);
-      shouldTriggerClick && !longPressTriggered && onClick(event);
-      setLongPressTriggered(false);
-      if (shouldPreventDefault && target.current) {
-        target.current.removeEventListener("touchend", preventDefault);
-      }
-    },
-    [shouldPreventDefault, onClick, longPressTriggered]
-  );
+  const clear = (event, shouldTriggerClick = true) => {
+    timeout.current && clearTimeout(timeout.current);
+    shouldTriggerClick && !longPressTriggered && onClick(event);
+    setLongPressTriggered(false);
+    if (shouldPreventDefault && target.current) {
+      target.current.removeEventListener("touchend", preventDefault);
+    }
+  };
 
   return {
     onMouseDown: (e) => start(e),
