261c74fbe430a4ea1758a461513e468f289fe7b8
Roman <roman.boikov@wsc-sports.com>
2025-01-24 21:54:17 +0100

    Removed redundant import

--- src/pages/Order/EditOrderModal/EditOrderModal.jsx
+++ src/pages/Order/EditOrderModal/EditOrderModal.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import { components } from "react-select";
 import Modal from "../../../components/common/Modal";
 import {
   EMAIL_REGEX,
